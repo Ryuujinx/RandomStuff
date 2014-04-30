c25edd2486755995bdc3fe0f8cbe4916ac213061
Dewey <admin@failverse.com>
2014-04-29 22:10:34 -0500

    Stuff

---
+++ fqdn.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+File.open("test-fqdn.txt","r") do |f|
+        f.each do |line|
+                arr = line.split(".")
+                if arr.count < 2
+                        puts "#{line.gsub("\n","")} is not an FQDN."
+                elsif arr.count == 2
+                        if line =~ /^.*\.(com|org|net)$/
+                                puts "#{line.gsub("\n","")} is an FQDN not ending in a dot."
+                        elsif line =~ /^.*\.(com|org|net)$/
+                                puts "#{line.gsub("\n","")} is an FQDN ending in a dot."
+                        else
+                                puts "#{line.gsub("\n","")} is not an FQDN."
+                        end
+                elsif arr.count > 2
+                        if arr[-1] =~ /(^com|org|net)$/
+                                if line =~ /^.*\.(com|org|net)$/
+                                         puts "#{line.gsub("\n","")} is an FQDN not ending in a dot."
+                                end
+                        elsif arr[-1] =~ /^(uk|cc)$/
+                                if line =~ /^.*\.(co\.uk|co\.cc)$/
+                                        puts "#{line.gsub("\n","")} is an FQDN not ending in a dot."
+                                end
+                        elsif line =~ /^.*\.(com|org|net)\.$/
+                                puts "#{line.gsub("\n","")} is an FQDN ending in a dot."
+                        elsif line =~ /^.*\.(co\.uk|co\.cc)\.$/
+                                puts "#{line.gsub("\n","")} is an FQDN ending in a dot."
+                        else
+                                puts "#{line.gsub("\n","")} is not an FQDN."
+                        end
+
+                end
+        end
+end
